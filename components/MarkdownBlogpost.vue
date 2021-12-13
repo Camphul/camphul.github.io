a13c982ede7bf04f61e21a4a3f5a522f7045e0ab
L.M. Camphuisen <luca.camphuisen@hva.nl>
2021-12-13 11:40:44 +0100

    line height

--- components/MarkdownBlogpost.vue
+++ components/MarkdownBlogpost.vue
@@ -3,7 +3,7 @@
     <div class="space-y-10 sm:space-y-12 lg:space-y-20 xl:space-y-24">
       <NuxtContent
         :document="blogPost"
-        class="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto leading-3"
+        class="prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto"
       />
     </div>
   </article>
