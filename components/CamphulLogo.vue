342153c2b7e7486a5fe67fc99c4137554e8f9cdc
Luca Camphuisen <Luca.Camphuisen@HVA.nl>
2021-12-13 05:00:14 +0100

    Base url

--- components/CamphulLogo.vue
+++ components/CamphulLogo.vue
@@ -1,19 +1,19 @@
 <template>
-  <a href="#" @click.prevent="$router.push('/index')">
+  <NuxtLink to="/index">
     <span class="pl-2 flex w-full justify-center items-center">
       <img id="camphul-logo" class="h-8 w-auto" content="eager" alt="logo" :src="baseUrlLogo">
       <p class="pl-2 font-medium"><slot></slot></p>
     </span>
-  </a>
+  </NuxtLink>
 </template>
 <script>
 export default {
   computed: {
     baseUrl () {
-      return (process.env.NODE_ENV !== 'production' ? process.env.baseUrl : 'https://camphul.github.io/Camphul')
+      return (process.env.NODE_ENV !== 'production' ? process.env.baseUrl : 'https://camphul.github.io/')
     },
     baseUrlLogo () {
-      return (process.env.NODE_ENV !== 'production' ? process.env.baseUrl : 'https://camphul.github.io/Camphul') + '/logo.png'
+      return (process.env.NODE_ENV !== 'production' ? process.env.baseUrl : 'https://camphul.github.io/') + '/logo.png'
     }
   }
 }
